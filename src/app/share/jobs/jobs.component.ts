814e9fccfac94eeafd0a574566af446df093bdfc
wuiancocos2d <wuiancocos2d@gmail.com>
2018-08-20 09:50:06 +0800

    job table
    job table

---
+++ src/app/share/jobs/jobs.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit} from '@angular/core';
+import {Job} from '../../core/models/job';
+
+@Component({
+  selector: 'app-jobs',
+  templateUrl: './jobs.component.html',
+  styleUrls: ['./jobs.component.css']
+})
+export class JobsComponent implements OnInit {
+
+  jobs: Job[];
+  tableData: Job[] = [...this.jobs];
+  filterStatusList = [
+    {text: 'Ready', value: 'Ready'},
+    {text: 'Running', value: 'Running'},
+    {text: 'Done', value: 'Done'},
+    {text: 'Error', value: 'Error'}
+  ];
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { NgZorroAntdModule } from 'ng-zorro-antd';
 import { ContainerComponent } from './container/container.component';
+import { JobsComponent } from './jobs/jobs.component';
+import { JobComponent } from './job/job.component';
+import { JobDetailComponent } from './job-detail/job-detail.component';
 @NgModule({
   imports: [
     CommonModule,
     NgZorroAntdModule,
   ],
-  declarations: [HeaderComponent, ContainerComponent],
+  declarations: [HeaderComponent, ContainerComponent, JobsComponent, JobComponent, JobDetailComponent],
   exports: [HeaderComponent, ContainerComponent, NgZorroAntdModule],
 })
 export class ShareModule { }
